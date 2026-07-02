3365b2a93bfc36f8127c9cf547419b6cbd99a929
Daniel Bergman <danielrbergman@gmail.com>
2026-07-02 15:03:44 -0400

    bump version to 0.5.0 and update SmoreGSA compatibility to 0.4 (#8)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,9 @@
 using Smore
 using Test
-import Pkg
 
 @testset "Smore" begin
     @test Smore isa Module
     @test isdefined(Smore, :SmoreBase)
     @test isdefined(Smore, :SmoreFit)
     @test isdefined(Smore, :SmoreGSA)
-end
-
-for name in ["SmoreBase", "SmoreFit", "SmoreGSA"]
-    Pkg.test(name)
 end
